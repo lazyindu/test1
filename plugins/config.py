750a650e2d1fc6456237f56458f59554d33de51e
indu <70087467+lazyindu@users.noreply.github.com>
2023-12-24 01:22:28 +0530

    Add files via upload

---
+++ plugins/config.py
@@ -0,0 +1,66 @@
+# with Love @LazyDeveloperr 💘
+# Subscribe YT @LazyDeveloperr - to learn more about this for free...
+
+import os
+
+import logging
+
+logging.basicConfig(
+    format='%(name)s - %(levelname)s - %(message)s',
+    handlers=[logging.FileHandler('log.txt'),
+              logging.StreamHandler()],
+    level=logging.INFO
+)
+
+class Config(object):
+
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+
+    API_ID = int(os.environ.get("API_ID", 12345))
+
+    API_HASH = os.environ.get("API_HASH")
+
+    AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "").split())
+
+    BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "").split())
+
+    DOWNLOAD_LOCATION = "./DOWNLOADS"
+
+    UPDATE_CHANNEL = os.environ.get("UPDATE_CHANNEL", "")
+
+    MAX_FILE_SIZE = 4194304000
+
+    TG_MAX_FILE_SIZE = 4194304000
+
+    FREE_USER_MAX_FILE_SIZE = 4194304000
+
+    CHUNK_SIZE = int(os.environ.get("CHUNK_SIZE", 128))
+
+    DEF_THUMB_NAIL_VID_S = os.environ.get("DEF_THUMB_NAIL_VID_S", "https://telegra.ph/file/0021415ce9c8d492ccd15.jpg")
+
+    HTTP_PROXY = os.environ.get("HTTP_PROXY", "")
+
+    OUO_IO_API_KEY = ""
+
+    MAX_MESSAGE_LENGTH = 4096
+
+    PROCESS_MAX_TIMEOUT = 0
+
+    DEF_WATER_MARK_FILE = ""
+
+    DATABASE_URI = os.environ.get("DATABASE_URI", "")
+
+    SESSION_NAME = os.environ.get("SESSION_NAME", "")
+
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", -100))
+
+    LOGGER = logging
+
+    OWNER_ID = int(os.environ.get("OWNER_ID", ""))
+
+    UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "")
+    
+    BOT_USERNAME = os.environ.get("BOT_USERNAME", "")
+    
+    # with Love @LazyDeveloperr 💘
+    # Subscribe YT @LazyDeveloperr - to learn more about this for free...
---
+++ plugins/rlazy_thumbnail.py
@@ -0,0 +1,144 @@
+    # Credit @LazyDeveloper.
+    # Please Don't remove credit.
+    # Born to make history @LazyDeveloper !
+
+    # Thank you LazyDeveloper for helping us in this Journey
+    # 🥰  Thank you for giving me credit @LazyDeveloperr  🥰
+
+    # for any error please contact me -> telegram@LazyDeveloperr or insta @LazyDeveloperr 
+
+from pyrogram import Client, filters
+from database.users_chats_db import db
+
+
+
+import logging
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
+import random
+import os
+from PIL import Image
+
+# the Strings used for this "thing"
+from pyrogram import Client
+
+from hachoir.metadata import extractMetadata
+from hachoir.parser import createParser
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+from pyrogram import filters
+from database.lazy_ffmpeg import take_screen_shot
+
+from database.ia_filterdb import db
+from plugins.config import Config
+from plugins.settings.settings import *
+
+
+@Client.on_message(filters.private & filters.command(['viewthumb']))
+async def viewthumb(client, message):    
+    thumb = await db.get_thumbnail(message.from_user.id)
+    if thumb:
+       await client.send_photo(
+	   chat_id=message.chat.id, 
+	   photo=thumb)
+    else:
+        await message.reply_text("😔**Sorry ! No thumbnail found...**😔") 
+
+
+@Client.on_message(filters.private & filters.command(['delthumb']))
+async def removethumb(client, message):
+    await db.set_thumbnail(message.from_user.id, file_id=None)
+    await message.reply_text("**Thumbnail deleted successfully**✅️")
+
+
+@Client.on_message(filters.private & filters.photo)
+async def addthumbs(client, message):
+    LazyDev = await message.reply_text("Please Wait ...")
+    await db.set_thumbnail(message.from_user.id, file_id=message.photo.file_id)                
+    await LazyDev.edit("**Thumbnail saved successfully**✅️")
+
+@Client.on_message(filters.private & filters.command(['view_url_thumb']))
+async def viewlazythumb(client, message):    
+    thumbnail = await db.get_lazy_thumbnail(message.from_user.id)
+    if not thumbnail:
+        await client.send_photo(
+        chat_id=message.chat.id, 
+        photo=thumbnail)
+    else:
+        await message.reply_text("😔**Sorry ! No thumbnail found...**😔")         
+
+@Client.on_message(filters.private & filters.command(['del_url_thumb']))
+async def removelazythumb(client, message):
+    await db.set_lazy_thumbnail(message.from_user.id, None)
+    await message.reply_text("**Thumbnail deleted successfully**✅️")
+
+@Client.on_message(filters.private & filters.photo & filters.command(['set_url_thumb']))
+async def addlazythumbs(client, message):
+    LazyDev = await message.reply_text("Please Wait ...")
+    await db.set_lazy_thumbnail(message.from_user.id, thumbnail=message.photo.file_id)                
+    await LazyDev.edit("**Thumbnail saved successfully**✅️")
+
+
+async def Gthumb01(bot, update):
+    thumb_image_path = Config.DOWNLOAD_LOCATION + "/" + str(update.from_user.id) + ".jpg"
+    db_thumbnail = await db.get_thumbnail(update.from_user.id)
+    if db_thumbnail is not None:
+        thumbnail = await bot.download_media(message=db_thumbnail, file_name=thumb_image_path)
+        Image.open(thumbnail).convert("RGB").save(thumbnail)
+        img = Image.open(thumbnail)
+        img.resize((100, 100))
+        img.save(thumbnail, "JPEG")
+    else:
+        thumbnail = None
+
+    return thumbnail
+
+async def Gthumb02(bot, update, duration, download_directory):
+    thumb_image_path = Config.DOWNLOAD_LOCATION + "/" + str(update.from_user.id) + ".jpg"
+    db_thumbnail = await db.get_thumbnail(update.from_user.id)
+    if db_thumbnail is not None:
+        thumbnail = await bot.download_media(message=db_thumbnail, file_name=thumb_image_path)
+    else:
+        thumbnail = await take_screen_shot(download_directory, os.path.dirname(download_directory), random.randint(0, duration - 1))
+
+    return thumbnail
+
+async def Mdata01(download_directory):
+
+          width = 0
+          height = 0
+          duration = 0
+          metadata = extractMetadata(createParser(download_directory))
+          if metadata is not None:
+              if metadata.has("duration"):
+                  duration = metadata.get('duration').seconds
+              if metadata.has("width"):
+                  width = metadata.get("width")
+              if metadata.has("height"):
+                  height = metadata.get("height")
+
+          return width, height, duration
+
+async def Mdata02(download_directory):
+
+          width = 0
+          duration = 0
+          metadata = extractMetadata(createParser(download_directory))
+          if metadata is not None:
+              if metadata.has("duration"):
+                  duration = metadata.get('duration').seconds
+              if metadata.has("width"):
+                  width = metadata.get("width")
+
+          return width, duration
+
+async def Mdata03(download_directory):
+
+          duration = 0
+          metadata = extractMetadata(createParser(download_directory))
+          if metadata is not None:
+              if metadata.has("duration"):
+                  duration = metadata.get('duration').seconds
+
+          return duration
