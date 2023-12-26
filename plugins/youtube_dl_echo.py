9004fb80d0d0df402e18f836e27dfa68ebdcdb18
indu <70087467+lazyindu@users.noreply.github.com>
2023-12-26 18:39:47 +0530

    Update youtube_dl_echo.py

--- plugins/youtube_dl_echo.py
+++ plugins/youtube_dl_echo.py
@@ -4,16 +4,55 @@ import logging
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from pyrogram import enums
-
+from info import *
 logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
 @Client.on_message(filters.private & filters.regex(pattern=".*http.*") & filters.incoming)
-async def echo(client, message):
+async def download_video(client, message):
     url = message.text
-
+    youtube_dl_username = None
+    youtube_dl_password = None
+    file_name = None
+    if "|" in url:
+        url_parts = url.split("|")
+        if len(url_parts) == 2:
+            url = url_parts[0]
+            file_name = url_parts[1]
+        elif len(url_parts) == 4:
+            url = url_parts[0]
+            file_name = url_parts[1]
+            youtube_dl_username = url_parts[2]
+            youtube_dl_password = url_parts[3]
+        else:
+            for entity in message.entities:
+                if entity.type == "text_link":
+                    url = entity.url
+                elif entity.type == "url":
+                    o = entity.offset
+                    l = entity.length
+                    url = url[o:o + l]
+        if url is not None:
+            url = url.strip()
+        if file_name is not None:
+            file_name = file_name.strip()
+        # https://stackoverflow.com/a/761825/4723940
+        if youtube_dl_username is not None:
+            youtube_dl_username = youtube_dl_username.strip()
+        if youtube_dl_password is not None:
+            youtube_dl_password = youtube_dl_password.strip()
+        logger.info(url)
+        logger.info(file_name)
+    else:
+        for entity in message.entities:
+            if entity.type == "text_link":
+                url = entity.url
+            elif entity.type == "url":
+                o = entity.offset
+                l = entity.length
+                url = url[o:o + l]
     command_to_exec = [
         "yt-dlp",
         "--no-warnings",
@@ -46,7 +85,22 @@ async def echo(client, message):
         if t_response:
             response_json = json.loads(t_response)
             inline_keyboard = []
-
+            # logger.info(t_response)
+            x_reponse = t_response
+            dlocation = DOWNLOAD_LOCATION
+            print(f"{dlocation}")
+            if "\n" in x_reponse:
+                x_reponse, _ = x_reponse.split("\n")
+            response_json = json.loads(x_reponse)
+            save_ytdl_json_path = DOWNLOAD_LOCATION + \
+                "/" + str(message.from_user.id) + ".json"
+            with open(save_ytdl_json_path, "w", encoding="utf8") as outfile:
+                json.dump(response_json, outfile, ensure_ascii=False)
+            # logger.info(response_json)
+            inline_keyboard = []
+            duration = None
+            if "duration" in response_json:
+                duration = response_json["duration"]
             if "formats" in response_json:
                 for formats in response_json["formats"]:
                     format_id = formats.get("format_id")
