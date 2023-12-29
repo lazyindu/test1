fefe12d36cf95cf7a0861505c238659dede07573
indu <70087467+lazyindu@users.noreply.github.com>
2023-12-30 00:34:01 +0530

    Update youtube_dl_button.py

--- plugins/youtube_dl_button.py
+++ plugins/youtube_dl_button.py
@@ -117,11 +117,11 @@ async def youtube_dl_call_back(client, query):
 
 
         # Use tqdm to display the progress bar
-        colored_bar_format = "{l_bar}" + "\033[32m{bar}\033[0m" + "{r_bar}"  # \033[32m sets the color to green
+        # colored_bar_format = "{l_bar}" + "\033[32m{bar}\033[0m" + "{r_bar}"  # \033[32m sets the color to green
         desc = f"ღ♡ʟᴀᴢʏ ᴄᴏɴꜱᴛʀᴜᴄᴛɪᴏɴ ɪꜱ ɢᴏɪɴɢ ᴏɴ♡♪**\n\n{custom_file_name}\n\n - 𝙴𝚗𝚓𝚘𝚢 𝚜𝚞𝚙𝚎𝚛𝚏𝚊𝚜𝚝 𝚍𝚘𝚠𝚗𝚕𝚘𝚊𝚍 𝚋𝚢 @LazyDeveloperr ◔_◔**"
         c_time = time.time()
         current_size = 0
-        with tqdm(total=xxLAZY_BAAPUxx, unit='B', unit_scale=True, unit_divisor=1024, desc=desc, bar_format=colored_bar_format) as bar:
+        with tqdm(total=xxLAZY_BAAPUxx, unit='B', unit_scale=True, unit_divisor=1024, desc=desc) as bar:
             def update_bar(chunk_size, xxLAZY_BAAPUxx):
                 bar.update(chunk_size)
 
@@ -130,7 +130,7 @@ async def youtube_dl_call_back(client, query):
                 current_size += chunk_size  # Increment current_size by the chunk_size
                 time.sleep(0.1)  # Simulate processing time
                 await progress_for_pyrogram2(current_size, xxLAZY_BAAPUxx, 'Download', xLAZY_BAAPUx_init, c_time, bar)
-   
+
            
             # def update_bar(chunk_size, xxLAZY_BAAPUxx):
             #     bar.update(chunk_size)
