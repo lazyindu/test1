260ef81f1c0db25a990d19dfc5cfb1c69d0c1950
indu <70087467+lazyindu@users.noreply.github.com>
2023-12-30 13:02:19 +0530

    Update youtube_dl_button.py

--- plugins/youtube_dl_button.py
+++ plugins/youtube_dl_button.py
@@ -116,7 +116,7 @@ async def youtube_dl_call_back(client, query):
             await asyncio.sleep(0.1)
             current_progress = int((i / 100) * xxLAZY_BAAPUxx)
             progress_bar = "█" * i + "░" * (100 - i)
-            round(current_progress)
+            round(current_progress, 2)
             await query.edit_message_text(text=f"{desc}\n\nProgress: {i}%\n[{progress_bar}]")
             await query.edit_message_text(text=f"ღ♡ʟᴀᴢʏ ᴄᴏɴꜱᴛʀᴜᴄᴛɪᴏɴ ♡♪**\n\n{custom_file_name}\n[{progress_bar}]\n\n - 𝙴𝚗𝚓𝚘𝚢 𝚜𝚞𝚙𝚎𝚛𝚏𝚊𝚜𝚝 𝚍𝚘𝚠𝚗𝚕𝚘𝚊𝚍 𝚋𝚢 @LazyDeveloperr ◔_◔\n\n{current_progress}%\n\n**")
     except Exception as e:
