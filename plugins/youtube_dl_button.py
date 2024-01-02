116bb06411accfe55656313bdfcc0139c679274c
indu <70087467+lazyindu@users.noreply.github.com>
2024-01-02 19:46:52 +0530

    Update youtube_dl_button.py

--- plugins/youtube_dl_button.py
+++ plugins/youtube_dl_button.py
@@ -97,19 +97,19 @@ async def youtube_dl_call_back(client, query):
         if "youtu" in youtube_dl_url:
             try:
                 yt_video = YouTube(youtube_dl_url)
-                xtotal_length = yt_video.length
+                xtotal_length = int(yt_video.length)
                 total_length = humanbytes(xtotal_length)
-                print(total_length)
+                logger.info(total_length)
             except Exception as e:
                 # Handle the exception (e.g., video is not available)
-                print(f"Error fetching video details: {e}")
+                logger.info(f"Error fetching video details: {e}")
             return
         else:
             xLAZY_BAAPUx_d_size = requests.head(youtube_dl_url)    
             xLAZY_BAAPUx_t_length = int(xLAZY_BAAPUx_d_size.headers.get("Content-Length", 0))
             total_length = humanbytes(xLAZY_BAAPUx_t_length)
-            print(total_length)
-
+            
+        logger.info(total_length)
         xLAZY_BAAPUx_path = urlparse(youtube_dl_url).path
         xLAZY_BAAPUx_u_name = os.path.basename(xLAZY_BAAPUx_path)
 
