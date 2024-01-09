08bf9b94e34ed4ed7fac788619174541da984df2
indu <70087467+lazyindu@users.noreply.github.com>
2024-01-10 00:25:32 +0530

    Update users_chats_db.py

--- database/users_chats_db.py
+++ database/users_chats_db.py
@@ -35,6 +35,7 @@ class Database:
             title = title,
             chat_status=dict(
                 is_disabled=False,
+                is_lazy_verified=False,
                 reason="",
             ),
         )
@@ -81,14 +82,15 @@ class Database:
 
     async def delete_user(self, user_id):
         await self.col.delete_many({'id': int(user_id)})
-
-
+                
     async def get_banned(self):
         users = self.col.find({'ban_status.is_banned': True})
         chats = self.grp.find({'chat_status.is_disabled': True})
+        is_verified = self.grp.find({'chat_status.is_lazy_verified': True})
         b_chats = [chat['id'] async for chat in chats]
         b_users = [user['id'] async for user in users]
-        return b_users, b_chats
+        lz_verified = [chat['id'] async for chat in is_verified]
+        return b_users, b_chats, lz_verified
 
     async def add_chat(self, chat, title):
         chat = self.new_group(chat, title)
@@ -128,6 +130,12 @@ class Database:
         chat_status=dict(
             is_disabled=True,
             reason=reason,
+            )
+        await self.grp.update_one({'id': int(chat)}, {'$set': {'chat_status': chat_status}})
+
+    async def verify_lazy_chat(self, chat):
+        chat_status=dict(
+            is_lazy_verified=True,
             )
         await self.grp.update_one({'id': int(chat)}, {'$set': {'chat_status': chat_status}})
     
