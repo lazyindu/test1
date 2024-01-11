a28c0c0a2c02844a399567be76731457abfa6911
indu <70087467+lazyindu@users.noreply.github.com>
2024-01-11 20:05:42 +0530

    Update info.py

--- info.py
+++ info.py
@@ -143,7 +143,7 @@ PRIME_DOWNLOADERS = (lazydownloaders) if lazydownloaders else []
 
 
 # Adding Language Feature : 
-LANGUAGES = ["malayalam", "mal", "tamil", "tam" ,"english", "eng", "hindi", "hin", "telugu", "tel", "kannada", "kan"]
+LANGUAGES = ["malayalam", "mal", "tamil", "tam" ,"english", "eng", "hindi", "hin", "telugu", "tel", "kannada", "kan","korean","kor","urdu","urd"]
 MAX_B_TN = environ.get("MAX_B_TN", "5")
 MAX_BTN = is_enabled((environ.get('MAX_BTN', "True")), True)
 
