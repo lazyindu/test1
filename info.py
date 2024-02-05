662836c96f8721db6853959c107b21c478d56194
indu <70087467+lazyindu@users.noreply.github.com>
2024-02-05 17:30:31 +0530

    Update info.py

--- info.py
+++ info.py
@@ -45,6 +45,7 @@ COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', 0))
+LAZY_GROUP_LOGS = int(environ.get('LAZY_GROUP_LOGS', 0))
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'LazyDeveloper')
 P_TTI_SHOW_OFF = is_enabled((environ.get('P_TTI_SHOW_OFF', "False")), False)
 IMDB = is_enabled((environ.get('IMDB', "True")), True)
